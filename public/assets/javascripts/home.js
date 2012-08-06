d592e8886b82c6569ee2c65ad5575cd8315d1983
Omer Buyukoglu <Omer@Omers-MacBook-Air.local>
2012-08-06 18:30:43 +0300

    Bug fixes, pixel perfect, detect price changes from option value, cool animations for quotes and like button.

--- public/assets/javascripts/home.js
+++ public/assets/javascripts/home.js
@@ -2,74 +2,76 @@
 
 $(document).ready(function() {
 
-        var aboveHeight = $('header').outerHeight();
-        function stickyNav() {
-                if ($(window).scrollTop() > aboveHeight){
-                        $('nav').addClass('fixed').css('top','0').next().css('padding-top','60px');
-                } else {
-                        $('nav').removeClass('fixed').next()
-                        .css('padding-top','0');
-                }
+    //Sticky Navbar
+    var hH = $('header').outerHeight();
+    function stickyNav() {
+        if ($(window).scrollTop() > hH){
+                $('nav').addClass('fixed').css('top','0').next().css('padding-top','60px');
+            }
+            else {
+                $('nav').removeClass('fixed').next().css('padding-top','0');
+            }
         }
         stickyNav();
-        $(window).scroll(function(){
-                stickyNav();
-        });
+        $(window).scroll(function(){stickyNav();
+    });
 
-
-        //QuoteSlides
-        $.fn.quotesSlide = function () {
-            var $this = this,
-            cur = 0,
-            fadeIt = function( which ) {
-                    var li = $this.find('li');
-                    cur = which = (which >= li.length) ? 0 : which;
-                    li.fadeOut(100);
-                    li.eq( which ).delay(100).fadeIn(1000, function(){
-                        setTimeout(function() {
-                            cur++;
-                            fadeIt( cur );
-                    }, 3000);
-                });
-            };
-            fadeIt(cur);
+    //QuoteSlides
+    $.fn.quotesSlide = function () {
+        var $this = this,
+        cur = 0,
+        fadeIt = function( which ) {
+                var li = $this.find('li');
+                cur = which = (which >= li.length) ? 0 : which;
+                li.fadeOut(100);
+                li.eq( which ).delay(100).fadeIn(1000, function(){
+                    setTimeout(function() {
+                        cur++;
+                        fadeIt( cur );
+                }, 4000);
+            });
+        };
+        fadeIt(cur);
     };
     $('ul.quotes').quotesSlide();
 
+    //*** Some cool stuff ***
+
+    //Show All Quotes
     $('a.view-all').click(function(){
         $('ul.quotes').hide();
         $('a.view-all').hide();
-        $('ul.hidden-quotes').fadeIn();
+        $('ul.hidden-quotes').fadeIn(800);
+        $('section#quotes').animate({'height': '600px'}, 500);
+    });
 
-    })
+    //Like Button
+    $('li.like').click(function(){
+        $('li.like').css('background', '#fe57a1');
+        $('span.liked').fadeIn();
+    });
 
+
+
+    //Price Changes
+    $("#prices").bind("change", function() {
+        var onPrice = $('#prices').val();
+        $('.price-change').html('Price: $ ' + onPrice);
+    });
 });
-
-$(document).ready(function() {
-        $("nav a").anchorAnimate()
-        wH = $(window).height();
-        $('#intro').css('height',wH+'px');
-        $("html,body").animate({ scrollTop: 0 }, 50);
+    
+//Scroll to Area
+$(function() {
+    $('nav ul li a').bind('click',function(event){
+        var $anchor = $(this);
+        var navH = $('nav').attr('class');
+        if (navH == 'fixed'){
+            $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top-30}, 800);
+            event.preventDefault();
+        }
+        else {
+            $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top-60}, 800);
+            event.preventDefault();
+        }
+    });
 });
-
-jQuery.fn.anchorAnimate = function(settings) {
-
-        settings = jQuery.extend({
-                speed : 1100
-        }, settings);   
-        
-        return this.each(function(){
-                var caller = this
-                $(caller).click(function (event) {      
-                        event.preventDefault()
-                        var locationHref = window.location.href
-                        var elementClick = $(caller).attr("href")
-                        
-                        var destination = $(elementClick).offset().top;
-                        $("html:not(:animated),body:not(:animated)").animate({ scrollTop: destination}, settings.speed, function() {
-                                window.location.hash = elementClick
-                        });
-                        return false;
-                })
-        })
-}
